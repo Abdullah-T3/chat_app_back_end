724d3524c75945a611c773137251d42bcb42d105
Abdullah-T3 <eng.abdullahahmed59@gmail.com>
2024-12-29 16:55:49 +0200

    //

--- app.js
+++ app.js
@@ -1,24 +1,26 @@
 const express = require('express');
-const dotenv = require('dotenv');
+const cors = require('cors');
+const messageRoutes = require('./routes/messageRoutes');
 const userRoutes = require('./routes/userRoutes');
 const chatRoutes = require('./routes/chatRoutes');
-const messageRoutes = require('./routes/messageRoutes');
 const postRoutes = require('./routes/postRoutes');
+require('dotenv').config();
 
-dotenv.config();
 const app = express();
+
+// Middleware
+app.use(cors());
 app.use(express.json());
 
 // Routes
+app.use('/api/messages', messageRoutes);
 app.use('/api/users', userRoutes);
 app.use('/api/chats', chatRoutes);
-app.use('/api/messages', messageRoutes);
 app.use('/api/posts', postRoutes);
 
-// Error Handling
-app.use((err, req, res, next) => {
-  console.error(err.stack);
-  res.status(500).json({ error: 'Something went wrong!' });
+// Default Route
+app.get('/', (req, res) => {
+  res.send('API is running...');
 });
 
 module.exports = app;
